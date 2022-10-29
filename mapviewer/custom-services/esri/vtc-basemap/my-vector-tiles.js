da61c91e31dd6702bc098c72de3632fbaa8d6f1f
jugstalt <gstalt@gmx.at>
2022-10-29 10:37:53 +0200

    custom services

--- mapviewer/custom-services/esri/vtc-basemap/my-vector-tiles.js
+++ mapviewer/custom-services/esri/vtc-basemap/my-vector-tiles.js
@@ -1,12 +1,19 @@
-webgis.events.on('viewer-map-created', function (channel, sender, map) {
+(function() {
+    let _customService;
 
-    var customService = map.addCustomService({
-        name: 'ESRI Basemap Topo',
-        frameworkElement: L.esri.Vector.vectorBasemapLayer('ArcGIS:Topographic', {
-            apikey: 'esri-api-key'
-        }),
-        isBasemap: true
-    }); 
+    webgis.events.on('before-map-add-services', function (channel, sender, map) {
+        _customService = map.addCustomService({
+            name: 'ESRI Basemap Topo',
+            frameworkElement: L.esri.Vector.vectorBasemapLayer('ArcGIS:Topographic', {
+                apikey: 'esri-api-key'
+            }),
+            isBasemap: true
+        }); 
 
-    map.setBasemap(customService.id);
-});
+        map.setBasemap(customService.id);
+    });
+
+    webgis.events.on('viewer-map-created', function(channel, sender, map) {
+        map.setBasemap(_customService.id);
+    });
+}());
--- mapviewer/custom-services/mapbox-gl/my-vector-tiles.js
+++ mapviewer/custom-services/mapbox-gl/my-vector-tiles.js
@@ -1,25 +1,38 @@
-webgis.events.on('viewer-map-created', function (channel, sender, map) {
+(function() {
+    let _firstCustomService;
 
-    var activeService = map.addCustomService({
-        name: 'VTC OSM Bright',
-        frameworkElement: L.mapboxGL({style: 'http://my-vector-tile-server/styles/osm-bright/style.json'}),
-        isBasemap: true,
-        fallback: 'vtc_fallback_osm_bright@custom'
+    webgis.events.on('after-map-add-services', function (channel, sender, map) {
+
+        _firstCustomService = map.addCustomService({
+            name: 'VTC OSM Bright',
+            frameworkElement: L.mapboxGL({style: 'http://my-vector-tile-server/styles/osm-bright/style.json'}),
+            isBasemap: true,
+            fallback: 'vtc_fallback_osm_bright@custom',
+            previewImageUrl: 'http://my-vector-tile-server/services/wmts/1.0.0/osm-bright/default/1x/16/22834/35141.png'
+        });
+
+        map.addCustomService({
+            name: 'VTC Dark Matter',
+            frameworkElement: L.mapboxGL({style: 'http://my-vector-tile-server/styles/dark-matter/style.json'}),
+            isBasemap: true,
+            previewImageUrl: 'http://my-vector-tile-server/services/wmts/1.0.0/dark-matter/default/1x/16/22834/35141.png'
+        });
+
+        map.addCustomService({
+            name: 'VTC Klokantech',
+            frameworkElement: L.mapboxGL({style: 'http://my-vector-tile-server/styles/klokantech-basic/style.json'}),
+            isBasemap: true,
+            fallback: 'geoland_bm@default',
+            previewImageUrl: 'http://my-vector-tile-server/services/wmts/1.0.0/klokantech-basic/default/1x/16/22834/35141.png'
+        });
     });
 
-    map.addCustomService({
-        name: 'VTC Dark Matter',
-        frameworkElement: L.mapboxGL({style: 'http://my-vector-tile-server/styles/dark-matter/style.json'}),
-        isBasemap: true
+    webgis.events.on('viewer-map-created', function(channel, sender, map) {
+        if(_firstCustomService) {
+            // turn off overlay basemaps (null => turn off, true => the overlay basemap)
+            map.setBasemap(null, true);
+            // show first custom base service
+            map.setBasemap(_firstCustomService.id, false);
+        }
     });
-
-    map.addCustomService({
-        name: 'VTC Klokantech',
-        frameworkElement: L.mapboxGL({style: 'http://my-vector-tile-server/styles/klokantech-basic/style.json'}),
-        isBasemap: true,
-        fallback: 'geoland_bm@default'
-    });
-
-    map.setBasemap(null, true);        // turn off overlay basemaps (null => turn off, true => the overlay basemap)
-    map.setBasemap(activeService.id);  // show first custom 
-});
+}());
